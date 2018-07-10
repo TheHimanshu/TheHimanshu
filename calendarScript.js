f62df0bedb35ded08019ee33f4061907eef8a8eb
Himanshu <him.star@yahoo.ca>
2018-07-09 23:15:53 -0700

    update calendar

--- calendarScript.js
+++ calendarScript.js
@@ -6,6 +6,9 @@ var daysInThisMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 var d = new Date();
 var divElementForCurrentMonth = d.getMonth() * 2; // because month has 2 div elements
 window.onload = function(){
+    var button = document.getElementById("myButton")
+    button.style.backgroundColor = color[randomNumber]
+    var title = document.getElementsByTagName("title")[0].innerText;
     var div = document.getElementsByTagName("div");
     var length = div.length;
     var boxesNotUsed;
@@ -18,10 +21,16 @@ window.onload = function(){
     daysOff = 2, threeDaysOff = false, // Number of days off first time in january
     //************************************
     
-    curMonth = false, clas = '', totalBoxes = 42, daysAWeek = 7, 
+    curMonth = false, clas = '', totalBoxes = 42, daysAWeek = 7,
+    dayNight = true;
     
     //*************Edit*******************
-    workDays = 2;// Number of days already worked in december last year
+    if (title == "Calendar"){
+        workDays = 2;// Number of days already worked in december last year
+    }
+    else {
+        workDays = -3;
+    }
     //************************************
     for(var i = 0; i < length; i++){ // for each month
         k = 1;
@@ -62,7 +71,12 @@ window.onload = function(){
                     threeDaysOff = true;
                 }
             }else{
-                day += "<li"+ clas +">" + k + "</li>";
+                if(dayNight && workDays < daysOff){
+                    day += "<li"+ clas +" style='background:white'>" + k + "</li>";
+                }   
+                else{
+                    day += "<li"+ clas +">" + k + "</li>";
+                }
                 k++;
                 workDays++;
             }
