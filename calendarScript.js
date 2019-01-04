3852c69e5cdcb8a1b205616b570ed2c156fb25c9
Himanshu <him.star@yahoo.ca>
2019-01-03 23:32:49 -0800

    update calendar

--- calendarScript.js
+++ calendarScript.js
@@ -4,24 +4,26 @@ var randomNumber = Math.floor((Math.random() * 7)); // random from 0 to 6
 var month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var daysInThisMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 var d = new Date();
-var divElementForCurrentMonth = d.getMonth() * 2; // because month has 2 div elements
+var dMonth = d.getMonth();
+var dYear = d.getFullYear();
+var divElementForCurrentMonth = dMonth * 2; // because month has 2 div elements
 window.onload = function(){
     var div = document.getElementsByTagName("div");
     var length = div.length;
     var boxesNotUsed;
     var j = 0, k = 1, 
     //*************Edit*******************
-    dateOneStartAfterDay = 0, // Jan 1 start on Monday then set it to 0 else calculate accordingly
+    dateOneStartAfterDay = (new Date("Jan 1, " + dYear)).getDay() - 1, // Jan 1 start on Monday then set it to 0 else calculate accordingly
     //************************************
     
-    //*************Edit*******************
-    daysOff = 2, threeDaysOff = false, // Number of days off first time in january
+    //*************Edit******************* 
+    daysOff = 3, threeDaysOff = true, // Number of days off first time in january
     //************************************
     
     curMonth = false, clas = '', totalBoxes = 42, daysAWeek = 7, 
     
     //*************Edit*******************
-    workDays = 2;// Number of days already worked in december last year
+    workDays = 0;// Number of days already worked in december last year
     //************************************
     for(var i = 0; i < length; i++){ // for each month
         k = 1;
@@ -71,7 +73,7 @@ window.onload = function(){
                                                     '<ul>' +
                                                         '<li style="text-align:center">' +
                                                             month[i] + '<br>' +
-                                                            '<span style="font-size:18px">2018</span>' +
+                                                            '<span style="font-size:18px">' + dYear + '</span>' +
                                                         '</li>' +
                                                     '</ul>' +
                                                 '</div>' +
